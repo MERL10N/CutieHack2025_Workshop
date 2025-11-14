0ea840fe2f42e9b548ccb316a9e1d67417613722
Kian <92978255+MERL10N@users.noreply.github.com>
2025-11-13 20:41:53 -0800

    Render shapes for pong game

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,30 +1,27 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
-
-#define WINDOW_TITLE "Window title"
-
 int main(void)
 {
+    constexpr int SCREEN_WIDTH = 1280;
+    constexpr int SCREEN_HEIGHT = 720;
+    const char* WINDOW_TITLE = "Cutiehack 2025 :D";
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(SKYBLUE);
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+        // Render shapes
+        DrawLine(SCREEN_WIDTH * 0.5f, 0, SCREEN_HEIGHT, WHITE);
+        DrawCircle(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 20, WHITE);
+        DrawRectangle(10, SCREEN_HEIGHT * 0.5f - 60, 25, 150, WHITE);
+        DrawRectangle(SCREEN_WIDTH - 35, SCREEN_HEIGHT * 0.5f - 60, 25, 150, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+
 
         EndDrawing();
     }
