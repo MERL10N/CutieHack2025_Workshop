12c35d7ca3a2721dc53531322aeecdb4a1538417
Kian <92978255+MERL10N@users.noreply.github.com>
2025-11-14 23:33:58 -0800

    Pong game final solution
    Code separated by class

---
+++ sources/ScoreSystem.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Kian Marvi on 11/14/25.
+//
+
+#include "ScoreSystem.h"
+#include "raylib.h"
+
+ScoreSystem::ScoreSystem()
+    : playerScore(0),
+      opponentScore(0)
+{
+}
+
+void ScoreSystem::AddPlayerScore()
+{
+    ++playerScore;
+}
+
+void ScoreSystem::AddOpponentScore()
+{
+    ++opponentScore;
+}
+
+void ScoreSystem::Draw(int screenWidth) const
+{
+    DrawText(TextFormat("%i", playerScore), screenWidth / 4 - 20, 20, 80, WHITE);
+    DrawText(TextFormat("%i", opponentScore), 3 * screenWidth / 4 - 20, 20, 80, WHITE);
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,28 @@
 #include "raylib.h"
 
+#include "Ball.h"
+#include "Paddle.h"
+#include "Opponent.h"
+#include "ScoreSystem.h"
+
 int main(void)
 {
     constexpr int SCREEN_WIDTH = 1280;
     constexpr int SCREEN_HEIGHT = 720;
-    const char* WINDOW_TITLE = "Cutiehack 2025 :D";
+    const char* WINDOW_TITLE = "Cutiehack 2025 :P";
+    Color Green = Color{38, 185, 154, 255};
+    Color Dark_Green = Color{20, 160, 133, 255};
+    Color Light_Green = Color{129, 204, 184, 255};
+    Color Yellow     = Color{243, 213, 91, 255};
+
+
+    Ball ball(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 20, 7, 7);
+
+    Paddle player(SCREEN_WIDTH - 25.0f - 10.0f,SCREEN_HEIGHT / 2.0f - 120.0f / 2.0f,25.0f,120.0f,6);
+    OpponentPaddle opponent(10.0f, SCREEN_HEIGHT / 2.0f - 120.0f / 2.0f,25.0f,  120.0f,  6);
+
+    ScoreSystem score;
+
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
@@ -13,15 +31,35 @@ int main(void)
     {
         BeginDrawing();
 
+        // Update
+        ball.Update(score);
+        player.Update();
+        opponent.Update(static_cast<int>(ball.GetY()));
+
+        // Checking for collisions
+        if (CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(),player.GetRect()))
+        {
+               ball.Bounce();
+        }
+
+        if (CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(),opponent.GetRect()))
+        {
+            ball.Bounce();
+        }
+
         ClearBackground(SKYBLUE);
 
         // Render shapes
-        DrawLine(SCREEN_WIDTH * 0.5f, 0, SCREEN_HEIGHT, WHITE);
-        DrawCircle(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 20, WHITE);
-        DrawRectangle(10, SCREEN_HEIGHT * 0.5f - 60, 25, 150, WHITE);
-        DrawRectangle(SCREEN_WIDTH - 35, SCREEN_HEIGHT * 0.5f - 60, 25, 150, WHITE);
+        ClearBackground(Dark_Green);
+        DrawRectangle(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, Green);
+        DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 150, Light_Green);
+        DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
 
-
+        // Render entities
+        ball.Draw(Yellow);
+        player.Draw();
+        opponent.Draw();
+        score.Draw(SCREEN_WIDTH);
 
         EndDrawing();
     }
